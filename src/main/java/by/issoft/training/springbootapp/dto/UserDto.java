71222ba5f20524e8aaf05c933bceda52cb02fa88
VladimirLyukhtenko <VladimirLyukhtenko@coherentsolutions.com>
2020-09-21 16:05:20 +0300

    update files

--- src/main/java/by/issoft/training/springbootapp/dto/UserDto.java
+++ src/main/java/by/issoft/training/springbootapp/dto/UserDto.java
@@ -1,2 +1,11 @@
-package by.issoft.training.springbootapp.dto;public class UserDto {
+package by.issoft.training.springbootapp.dto;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserDto {
+    private String name;
 }
