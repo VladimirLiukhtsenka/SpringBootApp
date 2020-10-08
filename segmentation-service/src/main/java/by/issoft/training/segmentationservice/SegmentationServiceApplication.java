b7c7fdbd6c2c593fcca297c1dde991f9dccbecd6
VladimirLyukhtenko <vladimirlyukhtenko@coherentsolutions.com>
2020-10-08 17:11:36 +0300

    AdvertisingContentRatingMinAgeConfig class added

--- segmentation-service/src/main/java/by/issoft/training/segmentationservice/SegmentationServiceApplication.java
+++ segmentation-service/src/main/java/by/issoft/training/segmentationservice/SegmentationServiceApplication.java
@@ -1,9 +1,12 @@
 package by.issoft.training.segmentationservice;
 
+import by.issoft.training.segmentationservice.config.AdvertisingContentRatingMinAgeConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AdvertisingContentRatingMinAgeConfig.class)
 public class SegmentationServiceApplication {
 
     public static void main(String[] args) {
--- segmentation-service/src/main/java/by/issoft/training/segmentationservice/service/AdvertisingContentRatingServiceImpl.java
+++ segmentation-service/src/main/java/by/issoft/training/segmentationservice/service/AdvertisingContentRatingServiceImpl.java
@@ -1,30 +1,25 @@
 package by.issoft.training.segmentationservice.service;
 
 import by.issoft.training.dto.UserDto;
+import by.issoft.training.segmentationservice.config.AdvertisingContentRatingMinAgeConfig;
 import by.issoft.training.segmentationservice.dto.AdvertisingContentRatingDto;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AdvertisingContentRatingServiceImpl implements AdvertisingContentRatingService {
-    @Value("${adContentRating.MinAge.generalAudiences}")
-    private int minAgeForGeneralAudiences;
-    @Value("${adContentRating.MinAge.parentalGuidance}")
-    private int minAgeForParentalGuidance;
-    @Value("${adContentRating.MinAge.teen}")
-    private int minAgeForTeen;
-    @Value("${adContentRating.MinAge.matureAudiences}")
-    private int minAgeForMatureAudiences;
+    private final AdvertisingContentRatingMinAgeConfig minAgeConfig;
 
     @Override
     public AdvertisingContentRatingDto determineRating(UserDto userDto) {
         Integer userAge = userDto.getAge();
         AdvertisingContentRatingDto ratingDto = new AdvertisingContentRatingDto();
 
-        ratingDto.setGeneralAudiences(userAge > minAgeForGeneralAudiences);
-        ratingDto.setParentalGuidance(userAge > minAgeForParentalGuidance);
-        ratingDto.setTeen(userAge > minAgeForTeen);
-        ratingDto.setMatureAudiences(userAge > minAgeForMatureAudiences);
+        ratingDto.setGeneralAudiences(userAge > minAgeConfig.getGeneralAudiences());
+        ratingDto.setParentalGuidance(userAge > minAgeConfig.getParentalGuidance());
+        ratingDto.setTeen(userAge > minAgeConfig.getTeen());
+        ratingDto.setMatureAudiences(userAge > minAgeConfig.getMatureAudiences());
 
         return ratingDto;
     }
